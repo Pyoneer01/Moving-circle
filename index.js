727397d20b58f05c0002538775f8cf0286f96406
Anish <139126949+Pyoneer01@users.noreply.github.com>
2023-09-05 21:21:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const initialTop = $("#box").css("top").slice(0,-2);
 
 let newLeft = parseInt(initialLeft);
 let newTop = parseInt(initialTop);
-let yDiff = Math.random(); //random value between 0 and 1
-let xDiff = Math.sqrt(1 - yDiff*yDiff); //random value between 0 and 1
+let yDiff = 2*Math.random() + 1; //random value between 1 and 3
+let xDiff = Math.sqrt(10 - yDiff*yDiff); //random value between 1 and 3
 
-const loop = setInterval(moveBox, 1);
+const loop = setInterval(moveBox, 10);
 
 function moveBox(){
-    if(newLeft + boxWidth + 10 >= innerWidth || newLeft < initialLeft){
+    if(newLeft + boxWidth + 2 >= innerWidth || newLeft < initialLeft){
         xDiff *= -1;
     }
-    if(newTop + boxWidth + 10 >= innerHeight || newTop < 10){
+    if(newTop + boxWidth + 2 >= innerHeight || newTop < 10){
         yDiff *= -1;
     }
     newLeft = newLeft + xDiff;
